1bc8adef06c87df04f27e6982d31805c00763326
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-07-10 19:51:28 -0500

    Pushing latest files. Having issues with data.json appearing at the moment.

--- Project_2/js/main.js
+++ Project_2/js/main.js
@@ -17,6 +17,33 @@ $(document).ready(function() {
 		confirmClear
 		//errMsg = ge('errors')
 	;
+
+	$('#jButton').on("click", function(){
+	// Load JSON data //
+		$('#dataView').empty();
+		$.ajax({
+			url: 'data/data.json',
+			type: 'GET',
+			dataType: 'json',	
+			success: function(response){
+				for (var i=0, j=response.workouts.length; i<j; i++){
+					console.log(response.workouts[i]);
+					var rw = response.workouts[i];
+					$(''+
+						'<div class="workoutData">'+
+							'<h2>'+ rw.training +'</h2>'+
+							'<p>'+ rw.wname +'</p>'+
+							'<p>'+ rw.favorite +'</p>'+
+							'<p>'+ rw.howlong +'</p>'+
+							'<p>'+ rw.timeofday +'</p>'+
+							'<p>'+ rw.completiondate +'</p>'+
+							'<p>'+ rw.comments +'</p>'+
+						'</div>'
+					).appendTo('#dataView');
+				};
+			}
+		});
+	});
 	
 	// getElementById function //
 	// function ge(x) {
